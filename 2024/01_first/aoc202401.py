9d1de6a7a682f8f478fba615be41c1cc27d8543a
Kristopher Beauchemin <kristhecanadian101@gmail.com>
2024-12-02 13:36:22 -0500

    day 1

--- 2024/01_first/aoc202401.py
+++ 2024/01_first/aoc202401.py
@@ -7,14 +7,46 @@ import sys
 
 def parse_data(puzzle_input):
     """Parse input."""
+    lines = []
+    right = []
+    left = []  
 
+    lines = puzzle_input.split("\n")
+
+    for line in lines:
+        line = line.strip()
+        split = line.split(' ')
+        left.append(int(split[0]))
+        right.append(int(split[-1]))
+    
+    return (left, right)
 
 def part1(data):
-    """Solve part 1."""
+    left = data[0]
+    right = data[1]
+
+    # sort from smallest to greatest
+    right.sort()
+    left.sort()
+    
+    distances = []
+
+    for index, _ in enumerate(left):
+        distances.append(abs(left[index] - right[index]))
+    
+    return sum(distances)
 
 
 def part2(data):
-    """Solve part 2."""
+    left = data[0]
+    right = data[1]
+
+    similarity = []
+    for num in left:
+        similarity.append(num * right.count(num))
+
+    return sum(similarity)
+    
 
 
 def solve(puzzle_input):
